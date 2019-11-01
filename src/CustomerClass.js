2d08e2740af5ac1425e9ed3e9ee7bcb815985bd1
mjtowarnicky+1@gmail.com <mjtowarnicky+1@gmail.com>
2019-10-31 21:02:32 -0600

    Add moved functions into parent class for later iterations

--- src/CustomerClass.js
+++ src/CustomerClass.js
@@ -31,5 +31,25 @@ class Customer {
     })
     return roomNumber
   }
+
+  todaysDate() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = yyyy + '/' + mm + '/' + dd;
+    return today;
+  }
+
+  booked(data, date) {
+    let result = data.filter(room => {
+      if (!date) {
+        return room.date === this.todaysDate();
+      }
+      return room.date === date
+
+    })
+    return result
+  }
 }
 export default Customer;
